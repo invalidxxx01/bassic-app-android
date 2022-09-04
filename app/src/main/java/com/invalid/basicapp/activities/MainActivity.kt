dfba7560fe339d2d2c721063149a25794d8857b6
invalidxxx01 <invalidxxx01@gmail.com>
2022-09-05 01:09:08 +0700

    bottom navigation tanpa view binding

--- app/src/main/java/com/invalid/basicapp/activities/MainActivity.kt
+++ app/src/main/java/com/invalid/basicapp/activities/MainActivity.kt
@@ -1,50 +1,27 @@
 package com.invalid.basicapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.invalid.basicapp.R
-import com.invalid.basicapp.ui.AccountFragment
-import com.invalid.basicapp.ui.DashboardFragment
-import com.invalid.basicapp.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
-	private val fm: FragmentManager = supportFragmentManager
-
-	private val fragmentHome: Fragment = HomeFragment()
-	private val fragmentDashboard: Fragment = DashboardFragment()
-	private var fragmentAccount: Fragment = AccountFragment()
+//	private lateinit var binding: ActivityMainBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+//		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(R.layout.activity_main)
 
-		setFragment(fragmentHome)
-		setBottomNav()
+//		val navView: BottomNavigationView = binding.navView
+		val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+		val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+		navView.setupWithNavController(navController)
 	}
-
-	private fun setFragment(fragment: Fragment) {
-		fm.beginTransaction().apply {
-			replace(R.id.nav_host, fragment)
-			commit()
-		}
-	}
-
-	private fun setBottomNav() {
-
-		val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-		bottomNavigationView.setOnItemSelectedListener { item ->
-
-			when (item.itemId) {
-				R.id.navigation_home -> setFragment(fragmentHome)
-				R.id.navigation_dashboard -> setFragment(fragmentDashboard)
-				R.id.navigation_account -> setFragment(fragmentAccount)
-			}
-			true
-		}
-	}
-
 }
